22731019352ddb10a97a2f7ff04cff3ed5157090
ARJUN N <138310139+ArjunN10@users.noreply.github.com>
2023-11-21 15:38:57 +0530

    second commit

--- models/AdminSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose")
-const adminschema=new mongoose.Schema({
-    username:"string",
-    email:"string",
-    password:"string"
-
-})
-
-module.exports=mongoose.model("admin",adminschema)
